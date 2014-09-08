9b9c30d7f0d900c3b2ea8c8bfa6bceafe351ca5d
Ben Christensen <benjchristensen@netflix.com>
2014-09-08 14:34:24 -0700

    CLI StartEurekaServer for Turbine with Eureka

--- turbine-ext/turbine-discovery-eureka/src/main/java/com/netflix/turbine/discovery/eureka/StartEurekaTurbine.java
+++ turbine-ext/turbine-discovery-eureka/src/main/java/com/netflix/turbine/discovery/eureka/StartEurekaTurbine.java
@@ -1,46 +1,78 @@
+/**
+ * Copyright 2014 Netflix, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.netflix.turbine.discovery.eureka;
 
-import io.netty.buffer.ByteBuf;
-import io.reactivex.netty.RxNetty;
-import io.reactivex.netty.pipeline.PipelineConfigurators;
-import io.reactivex.netty.protocol.text.sse.ServerSentEvent;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.netflix.turbine.Turbine;
-import com.netflix.turbine.internal.JsonUtility;
 
 public class StartEurekaTurbine {
+    private static final Logger logger = LoggerFactory.getLogger(StartEurekaTurbine.class);
 
     public static void main(String[] args) {
-        //        Turbine.aggregateHttpSSE(
-        //                EurekaStreamDiscovery.create("api", "http://"
-        //                        + EurekaStreamDiscovery.HOSTNAME
-        //                        + ":8077/eventbus.stream?topic=hystrix-metrics&delay=1000"))
-        //                .flatMap(o -> o)
-        //                .map(data -> JsonUtility.mapToJson(data))
-        //                .toBlocking()
-        //                .forEach(System.out::println);
-        //        
-        //        
-        System.out.println("Starting server on 8888");
-        RxNetty.createHttpServer(8888, (request, response) -> {
-            System.out.println("Starting aggregation => " + request.getUri());
-            System.out.println("headers: " + response.getHeaders());
-            response.getHeaders().setHeader("Content-Type", "text/event-stream");
-            return Turbine.aggregateHttpSSE(
-                    EurekaStreamDiscovery.create("api", "http://"
-                            + EurekaStreamDiscovery.HOSTNAME
-                            + ":8077/eventbus.stream?topic=hystrix-metrics&delay=1000"))
-                    .doOnUnsubscribe(() -> System.out.println("Unsubscribing RxNetty server connection"))
-                    .filter(o -> o.getKey().getKey().contains("Geo"))
-                    .flatMap(o -> o)
-                    .flatMap(data -> {
-                        return response.writeAndFlush(new ServerSentEvent(null, null, JsonUtility.mapToJson(data)));
-                    }).doOnError(t -> {
-                        t.printStackTrace();
-                        response.close(); // this shouldn't be needed
-                        });
-        }, PipelineConfigurators.<ByteBuf> sseServerConfigurator()).startAndWait();
+        OptionParser optionParser = new OptionParser();
+        optionParser.accepts("port").withRequiredArg();
+        optionParser.accepts("app").withRequiredArg();
+        optionParser.accepts("urlTemplate").withRequiredArg();
 
+        OptionSet options = optionParser.parse(args);
+        int port = -1;
+        if (!options.has("port")) {
+            System.err.println("Argument -port required for SSE HTTP server to start on. Eg. -port 8888");
+            System.exit(-1);
+        } else {
+            try {
+                port = Integer.parseInt(String.valueOf(options.valueOf("port")));
+            } catch (NumberFormatException e) {
+                System.err.println("Value of port must be an integer but was: " + options.valueOf("port"));
+            }
+        }
+
+        String app = null;
+        if (!options.has("app")) {
+            System.err.println("Argument -app required for Eureka instance discovery. Eg. -app api");
+            System.exit(-1);
+        } else {
+            app = String.valueOf(options.valueOf("app"));
+        }
+
+        String template = null;
+        if (!options.has("urlTemplate")) {
+            System.err.println("Argument -urlTemplate required. Eg. http://" + EurekaStreamDiscovery.HOSTNAME + "/metrics.stream");
+            System.exit(-1);
+        } else {
+            template = String.valueOf(options.valueOf("urlTemplate"));
+            if (!template.contains(EurekaStreamDiscovery.HOSTNAME)) {
+                System.err.println("Argument -urlTemplate must contain " + EurekaStreamDiscovery.HOSTNAME + " marker. Eg. http://" + EurekaStreamDiscovery.HOSTNAME + "/metrics.stream");
+                System.exit(-1);
+            }
+        }
+
+        logger.info("Turbine => Eureka App: " + app);
+        logger.info("Turbine => Eureka URL Template: " + template);
+
+        try {
+            Turbine.startServerSentEventServer(port, EurekaStreamDiscovery.create(app, template));
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
     }
 
 }
