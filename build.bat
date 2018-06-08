749b55858f19ae18a3b38319fabf69c07df1c3c1
Sinstraliz <blagovest.petrov@eldersoss.com>
2018-06-08 15:42:23 +0300

    Remove build and start bat files

--- build.bat
+++
@@ -1,2 +0,0 @@
-cd %~dp0
-.\gradlew.bat shadowJar %*
--- start-turbine.bat
+++
@@ -1,3 +0,0 @@
-echo off
-cd %~dp0
-java -cp .\turbine-ext\turbine-discovery-consul\build\libs\turbine-discovery-consul-executable-2.0.0-DP.3-SNAPSHOT.jar com.netflix.turbine.discovery.consul.StartConsulTurbine %*
